43f5375a4f10338379b573f172453acd52a4c194
liyijie <liyijie825@gmail.com>
2012-09-25 23:15:56 +0800

    Validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should check the attribute validation" do
+  	product = Product.new(:title => "test", :description => "yyy")
+  	product.should_not be_valid
+  	
+  	product.image_url = "test.gif"
+  	product.price = -1
+  	product.should_not be_valid
+
+  	product.price = 0
+  	product.should_not be_valid
+
+  	product.price = 5
+  	product.should be_valid
+  end
 end
